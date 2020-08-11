2c0dfe7dc5a21d7e85e4910182f07c55668a79a5
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-08-11 18:03:54 +0300

    frontend login and create

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
-    <h2 class="title">Create new User</h2>
+    <h2 class="title">Auth in user</h2>
     <el-form ref="form" :model="form" :rules="rules">
-      <el-form-item prop="email">
+      <el-form-item prop="email" label="email">
         <el-input v-model="form.email" placeholder="email" />
       </el-form-item>
-      <el-form-item prop="password">
+      <el-form-item prop="password" label="password">
         <el-input
           v-model="form.password"
           placeholder="password"
@@ -13,16 +13,8 @@
           show-password
         />
       </el-form-item>
-      <el-form-item prop="confirm">
-        <el-input
-          v-model="form.confirm"
-          placeholder="confirm"
-          type="password"
-          show-password
-        />
-      </el-form-item>
-      <el-button @click="create">Create</el-button>
-      <nuxt-link to="login">login</nuxt-link>
+      <el-button @click="auth">Auth</el-button>
+      <nuxt-link to="/">back</nuxt-link>
     </el-form>
   </div>
 </template>
@@ -30,47 +22,25 @@
 <script>
 export default {
   data() {
-    const validatePass = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('Please input the password'))
-      } else {
-        if (this.form.confirm !== '') {
-          this.$refs.form.validateField('checkPass')
-        }
-        callback()
-      }
-    }
-    const validatePass2 = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('Please input the password again'))
-      } else if (value !== this.form.password) {
-        callback(new Error("Two inputs don't match!"))
-      } else {
-        callback()
-      }
-    }
     return {
       form: {
         email: '',
         password: '',
-        confirm: '',
       },
       rules: {
         email: [{ required: true }],
-        password: [{ validator: validatePass, trigger: 'blur' }],
-        confirm: [{ validator: validatePass2, trigger: 'blur' }],
+        password: [{ required: true }],
       },
     }
   },
   methods: {
-    create() {
+    auth() {
       this.$refs.form.validate((valid) => {
         if (valid) {
           // eslint-disable-next-line
           const form = {
             email: this.form.email,
             password: this.form.password,
-            confirm: this.form.confirm,
           }
         } else {
           // eslint-disable-next-line
