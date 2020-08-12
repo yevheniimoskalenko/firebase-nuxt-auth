d8361ee3f47a59bea588840773991ed5a31d4304
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-08-12 20:01:40 +0300

    push todo in firebase realtime

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
     <div v-if="user" class="email">
-      {{ user.email }}
+      p
+      <span>
+        <i class="el-icon-user"></i>
+        {{ user.email }}
+      </span>
       <el-button @click="logOut">log out</el-button>
+    </div>
+    <div class="todo">
+      <div class="create-todo">
+        <el-form ref="form" :rules="rules" :model="form">
+          <el-form-item prop="todo">
+            <el-input v-model="form.todo" placeholder="new todo" />
+            <el-button icon="el-icon-plus" circle @click="addTodo" />
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="todo-list">
+        <div class="todo-item"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,20 +41,35 @@ export default {
       }
     }
   },
+  data() {
+    return {
+      form: { todo: '' },
+      rules: {},
+    }
+  },
   methods: {
     async logOut() {
       await this.$fireAuth
         .signOut()
         .then(() => this.$router.replace({ name: 'login' }))
     },
+    async addTodo() {
+      const todos = this.$fireDb.ref('todos')
+      try {
+        await todos.push({
+          todo: this.form.todo,
+          date: Date.now(),
+          uid: this.user.user_id,
+        })
+        this.$message({ message: 'todo create', type: 'success' })
+      } catch (e) {
+        this.$message({
+          message: `${e.message}`,
+          type: 'warning',
+        })
+      }
+    },
   },
-  // mounted() {
-  //   this.$fireAuth.currentUser
-  //     .getIdToken(true)
-  //     .then((token) => console.log(token))
-
-  //   // this.$fireAuth.onAuthStateChanged((user) => console.log(user))
-  // },
 }
 </script>
 
